a7e5664a769324ee90260da38648a96799b48952
DaVitoria <103380996+DaVitoria@users.noreply.github.com>
2026-01-08 18:35:53 +0200

    Config assets.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sgees.app',
+  appName: 'sgees',
+  webDir: 'dist'
+};
+
+export default config;
